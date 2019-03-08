d596fe89461822dcbfdc6abebec2e800ae01f788
Erik <erik.abikhattar@gmail.com>
2019-03-07 21:28:25 -0500

    adds inline model admin for entries on clients

--- biometrics/admin.py
+++ biometrics/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from biometrics.models import Client, Entry
 
 
+class InlineEntryAdmin(admin.TabularInline):
+    model = Entry
+
+
 class ClientAdmin(admin.ModelAdmin):
     readonly_fields = ('metrics_link',)
+    inlines = [InlineEntryAdmin]
 
 
 admin.site.register(Client, ClientAdmin)

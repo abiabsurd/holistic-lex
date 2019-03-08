814c21fdec50a9ed0442ee6180b33e77fb0a0de9
Erik <erik.abikhattar@gmail.com>
2019-03-07 19:45:15 -0500

    adds sentry

--- holistic_lex/settings/prod.py
+++ holistic_lex/settings/prod.py
@@ -1,8 +1,17 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from holistic_lex.settings.base import *
+
 
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+sentry_sdk.init(
+    dsn='https://cec38e4b1bde476382d69d157fe709b5@sentry.io/1410719',
+    integrations=[DjangoIntegration()]
+)
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
